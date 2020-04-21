012cb954b48989a1fdb3447d759a7fb47a7fc8d4
Sam <samstevens.ohio@gmail.com>
2020-04-20 20:05:44 -0400

    drawing rectangles

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
     console.log("starting");
 
     var drawInProgress = false;
-    var currentRect = new Rect;
-    var currentRectangle = new Rectangle();
+    var currentRectangle = new Rectangle;
     var canvas = document.getElementById("TheCanvas");
     var context = canvas.getContext("2d");
     var infoDisplay = document.getElementById("InfoDisplay");
@@ -16,41 +15,62 @@
      console.log("hello", e);
      drawInProgress = true;
 
-     currentRect.x = e.clientX - canvas.offsetLeft;
-     currentRect.y = e.clientY - canvas.offsetTop;
-     // todo: set width and height from mouse move delta
-     currentRect.width = 10;
-     currentRect.height = 100;
-
-     drawLine(rect.x, rect.y, rect.width, rect.height);
+     currentRectangle = new Rectangle;
+     currentRectangle.topLeft = new Point(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+     
+     drawLine(currentRectangle.topLeft.x, currentRectangle.topLeft.y, 10, 100);
    };
    canvas.onmouseup = function(e) {
      drawInProgress = false;
      
+     var x = e.clientX - canvas.offsetLeft;
+     var y = e.clientY - canvas.offsetTop;
 
+     if(x < currentRectangle.topLeft.x)
+        x = currentRectangle.topLeft.x;
+     if(y < currentRectangle.topLeft.y)
+        y = currentRectangle.topLeft.y;
+
+     currentRectangle.bottomRight = new Point(x, y);
+     currentRectangle.bottomLeft = new Point(currentRectangle.topLeft.x, y);
+     currentRectangle.topRight = new Point(x, currentRectangle.topLeft.y);
+
+     drawRectangle(currentRectangle);
    };
    canvas.onmousemove = function(e) {
      //console.log("onmousemove", e);
      if(drawInProgress) {
 
      }
-     
-     var rect = new Rect;
-     rect.x = e.clientX - canvas.offsetLeft;
-     rect.y = e.clientY - canvas.offsetTop;
-     rect.width = 10;
-     rect.height = 10;
 
-     infoDisplay.innerHTML = "X: " + rect.x + " Y: " + rect.y;
+     var x = e.clientX - canvas.offsetLeft;
+     var y = e.clientY - canvas.offsetTop;
+     var point = new Point(x, y);
+
+     infoDisplay.innerHTML = "X: " + point.x + " Y: " + point.y;
    };
 
    /*
-   *  Draw a line at the x and y coordinates.
+   *  Draw the outline of a rectangle.
    */
-   function drawLine(x, y, width, height) {
+   function drawRectangle(rectangle) {
+    // draw left line
+    drawLine(rectangle.topLeft, rectangle.bottomLeft);
+    // draw top line
+    drawLine(rectangle.topLeft, rectangle.topRight);
+    // draw bottom line
+    drawLine(rectangle.bottomLeft, rectangle.bottomRight);
+    // draw right line
+    drawLine(rectangle.bottomRight, rectangle.topRight);
+   }
+
+   /*
+   *  Draw a line to connect two points.
+   */
+   function drawLine(pointOne, pointTwo) {
       context.beginPath();
-      context.moveTo(x, y);
-      context.lineTo(x + width, y + height);
+      context.moveTo(pointOne.x, pointOne.y);
+      context.lineTo(pointTwo.x, pointTwo.y);
       context.stroke();
       context.closePath();
    }
@@ -58,19 +78,16 @@
    //
    // object prototypes
    //
-   function Rect() {
-       this.x = 0;
-       this.y = 0;
-       this.width = 0.0;
-       this.height = 0.0;
-   }
-
    function Rectangle() {
-     this.topLeft = new Point();
-     this.bottomLeft = new Point();
-     this.topRight = new Point();
-     this.bottomRight = new Point();
+     this.topLeft = new Point(0,0);
+     this.bottomLeft = new Point(0,0);
+     this.topRight = new Point(0,0);
+     this.bottomRight = new Point(0,0);
    }
 
+   function Point(x, y) {
+     this.x = x;
+     this.y = y;
+   }
 
 })();
